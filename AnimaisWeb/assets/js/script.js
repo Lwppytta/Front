3320f02b92c1d37c3f6d49ddaa6cc8ee77093f9f
Lwppytta <amandalwppytta@gmail.com>
2023-10-31 10:38:03 -0300

    JS

--- AnimaisWeb/assets/js/script.js
+++ AnimaisWeb/assets/js/script.js
@@ -29,3 +29,17 @@ function validarEmail(){
         emailOk= true
     }
 }
+
+let mensagemOk = false
+function validarMensagem(){
+    let txtMensagem = document.querySelector('#txtMensagem')
+    if(mensagem.value.length >= 100){
+        txtMensagem.innerHTML= "Texto muito grande, digite no maximo 100 caracteres."
+        txtMensagem.style.color = 'red'
+        txtMensagem.style.display = 'block'
+        mensagemOk= false
+    } else{
+        txtMensagem.style.display = 'none'
+        mensagemOk= true
+    }
+}
